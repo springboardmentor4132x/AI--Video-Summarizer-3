19a90f271e618f002d596cc416d65fab03ae01b4
Pragati Shaw <pragatishaw108@gmail.com>
2026-09-20 00:04:26 +0530

    Fixed main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,24 +4,14 @@ import './index.css'
 import './theme.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext'
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
-=======
->>>>>>> 6405c7b05fdff16e70d270d71c6b9e81127d6f7e
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-<<<<<<< HEAD
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ThemeProvider>
   </StrictMode>,
 );
-=======
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
-)
->>>>>>> 6405c7b05fdff16e70d270d71c6b9e81127d6f7e
